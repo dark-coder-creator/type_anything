cbf5ad9d3008782644df43ec3992f90dee9dfab0
Cheloor Saravanan Mohandas <saravanan.mohandas24@gmail.com>
2022-03-10 12:13:58 +0530

    added pages and router dom for different routings

---
+++ Frontend/typeanything/src/pages/Admin/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Admin.css'
+
+ const Admin = () => {
+  return (
+    <div>
+        This is Admin Page
+    </div>
+  )
+}
+
+
+export default Admin
---
+++ Frontend/typeanything/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import Content from '../../components/Content/Content'
+import Footer from '../../components/Footer/Footer'
+ const Home = () => {
+  return (
+    <div>
+        <Header />
+      <Content />
+      <Footer />  
+    </div>
+  )
+}
+
+
+export default Home
