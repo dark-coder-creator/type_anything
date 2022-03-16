dc6e6f9da08c127ac9b47455d397cfdf7b2365ac
Cheloor Saravanan Mohandas <saravanan.mohandas24@gmail.com>
2022-03-16 18:11:40 +0530

    added navbar

---
+++ Frontend/typeanything/src/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='Navbar'>
+        <div className='leftSide'>
+             ADMIN.
+        </div>
+    </div>
+  )
+}
